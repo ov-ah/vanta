1a8ba7c52fdfad3cf29d942eda2f01052c3a9b8e
ovah <admin@ovah.dev>
2026-04-30 15:58:58 -0700

    use 2d and renderer fixes

--- src/renderer.c
+++ src/renderer.c
@@ -7,9 +7,7 @@ static ShaderProgram shaderProgram;
 
 bool renderer_init(void)
 {
-	float vertices[] = {
-	    -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f,
-	};
+	float vertices[] = {-0.5f, -0.5f, 0.5f, -0.5f, 0.0f, 0.5f};
 	vertexCount = 3;
 
 	glGenVertexArrays(1, &vao);
@@ -19,8 +17,8 @@ bool renderer_init(void)
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_ARRAY_BUFFER, sizeof(vertices),
-	                      3 * sizeof(float), (void *)0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float),
+	                      (void *)0);
 	glEnableVertexAttribArray(0);
 
 	Shader vert, frag;
@@ -47,7 +45,7 @@ bool renderer_init(void)
 
 void renderer_begin_frame(void)
 {
-	glClearColor(1.0f, 1.0f, 0.1f, 1.0f);
+	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 }
 
